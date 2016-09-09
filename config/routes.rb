8e66fc7d373d9f9fca3fdb4c8302b8193c0d4cc0
Noah Ramey <noahramey1997@gmail.com>
2016-09-09 11:07:33 -0700

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root :to => 'playlists#index'
 
   resources :playlists, :except => [:edit, :update, :destroy]
+
+  get '/auth/spotify/callback', to: 'users#spotify'
 end
